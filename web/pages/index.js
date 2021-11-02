f8476207dd2f88c2bb2e2c16ae30dfaa17b9df4b
Marius Nettum <nettum@gmail.com>
2021-11-02 10:02:26 +0100

    Fix crossOrigin attribute to avoid warning

--- web/pages/index.js
+++ web/pages/index.js
@@ -23,7 +23,7 @@ export default function Home(props) {
         <meta name="description" content="A list of all the gigs I've attended" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet" />
       </Head>
 
