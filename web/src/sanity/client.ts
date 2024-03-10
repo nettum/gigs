c4143a72e8f6a8ea90be006897ec362a1204f6c4
Marius Nettum <nettum@gmail.com>
2024-03-10 23:36:42 +0100

    Upgrade sanity apiVersion

--- web/src/sanity/client.ts
+++ web/src/sanity/client.ts
@@ -1,8 +1,8 @@
-import { createClient } from "@sanity/client";
+import { createClient } from '@sanity/client';
 
 export default createClient({
-  projectId: "n4t6nimq",
-  dataset: "production",
+  projectId: 'n4t6nimq',
+  dataset: 'production',
   useCdn: true,
-  apiVersion: "2021-08-31",
+  apiVersion: '2024-03-10',
 });
